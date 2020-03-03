2d498cb1fa743614f0e94104ec806fd8e1336f30
hkmiari <60976284+hkmiari@users.noreply.github.com>
2020-03-03 13:12:00 +0100

    Implemented more test cases

--- Test/Test.java
+++ Test/Test.java
@@ -1,8 +1,6 @@
 import fitness_app.core.Person;
 import org.junit.jupiter.api.Test;
 
-import java.util.logging.SocketHandler;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -11,14 +9,32 @@ class test {
     Person hussein = new Person("Hussein", "Miari", 92, 190, 20, "male"
             ,"Denmark", "Sjælland", "Smørum", "Erantishaven 4");
     @Test
+    public void emptyPersonTest(){
+        Person emptyPerson = new Person();
+        assertEquals(emptyPerson.getAge(), -1);
+        assertEquals(emptyPerson.getWeight(), -1);
+        assertEquals(emptyPerson.getHeight(), -1);
+        assertEquals(emptyPerson.getFirstName(), "");
+    }
+
+    @Test
+    public void createNewPersonTest(){
+     Person anton = new Person("Anton", "Due", 98, 216, 21, "Male", "Denmark", "Sjælland", "Roskilde", "Pas");
+     assertEquals(anton.getAge(), 21);
+     anton.setAge(22);
+     assertEquals(anton.getAge(), 22);
+    }
+
+    @Test
+    public void getFieldFromConstructorTest(){
+        assertEquals(hussein.getAge(), 20);
+    }
+
+    @Test
     public void setterTest(){
         hussein.print_person_details();
         hussein.setAge(21);
         hussein.print_person_details();
         assertEquals(21, hussein.getAge());
-    }
-    @Test
-    public void emptyPersonTest(){
-
     }
 }
