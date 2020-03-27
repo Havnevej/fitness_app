e695b2939432704a127ed62a8387c1698985f736
havnevej <antondue8@gmail.com>
2020-03-27 13:59:49 +0100

    Refactored client to use switch statements for checking user input

--- src/fitness_app/core/Client.java
+++ src/fitness_app/core/Client.java
@@ -1,27 +1,20 @@
 package fitness_app.core;
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.lang.reflect.Type;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Period;
+
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Client {
     public static ArrayList<Person> list_with_people = new ArrayList<>();
     private static ArrayList<String> commands = new ArrayList<>(Arrays.asList("QUIT",
-            "CREATE_PERSON",
+            "CREATE PERSON",
+            "DELETE PERSON",
             "LOGIN"));
 
 
     public static void main(String[] args) {
+        main_loop();
         while(!login_user()){
 
         }
-        main_loop();
     }
 
     private static boolean login_user() {
@@ -29,29 +22,55 @@ public class Client {
         return false;
     }
 
+    private static void print_commands_available(){
+        System.out.println("\nCommands:");
+        for(String s : commands){
+            System.out.printf("|%s", s);
+        }
+        System.out.print("|\n");
+    }
+
     private static void main_loop()  {
-        //needs cleanup
         Scanner input_reader = new Scanner(System.in);
-        System.out.println("To create a person type: 'create person'");
         try {
             while(true){
+                print_commands_available();
                 String input = input_reader.nextLine().toUpperCase();
-                if(commands.contains(input)){
-                    if(input.equals("QUIT")){System.exit(0);} // quit
-                    else if(input.equals("CREATE PERSON")){
-                        Database_functions.create_person_from_user_input(); //need implementation
-                    } else if(input.equals("DELETE PERSON")){
-                        Database_functions.delete_person(); //need implementation
-                    } else if(input.equals("LOGIN")){
-                        Database_functions.login_user(); //need implementation
-                    }
-
+                switch (input){
+                    case "QUIT":
+                        System.exit(0);
+                        break;
+                    case "CREATE PERSON":
+                        Database_functions.create_person_from_user_input();
+                        break;
+                    case "DELETE PERSON":
+                        System.out.print("delete user by: 'email'|'id'\n:");
+                        input = input_reader.nextLine().toUpperCase();
+                        switch (input) {
+                            case "ID":
+                                System.out.print("id: ");
+                                int id = input_reader.nextInt();
+                                Database_functions.delete_person_by_id(id);
+                                break;
+                            case "EMAIL":
+                                System.out.print("email: ");
+                                String email = input_reader.nextLine();
+                                Database_functions.delete_person_by_email(email);
+                                break;
+                            default:
+                                System.out.println("Input should be 'email' or 'id'");
+                            }
+                        break;
+                    case "LOGIN":
+                        Database_functions.login_user();
+                        break;
+                    default:
+                        System.out.println("Command not found");
                 }
-
             }
         }
         catch (InputMismatchException e){ //InputMismatchException
-            System.out.println("Error: " + e + " Input can only be numbers" );
+            System.out.println("Error: " + e + " Input mismatch" );
         }
     }
 }
