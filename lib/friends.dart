7fe11253c6648c35fc5a3e315cad5fa5ad98012b
Hammi007 <H.e.4000@hotmail.com>
2020-05-12 18:19:55 +0200

    Friends and search page design and implementaion (in progress).

--- lib/friends.dart
+++ lib/friends.dart
@@ -1,145 +1,90 @@
-import 'dart:math';
-
-import 'package:flappy_search_bar/flappy_search_bar.dart';
-import 'package:flappy_search_bar/scaled_tile.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_fitness_app/constants.dart';
+import 'package:flutter_fitness_app/search.dart';
 
-
-class Post {
-  final String title;
-  final String body;
-
-  Post(this.title, this.body);
-}
-class Friends extends StatefulWidget {
-  final String title;
-  final String body;
-  const Friends({Key key, this.title, this.body}) : super(key: key);
-
-  @override
-  _FriendsState createState() => _FriendsState();
-}
-
-class _FriendsState extends State<Friends> {
-
-  final SearchBarController<Post> _searchBarController = SearchBarController();
-  bool isReplay = false;
-
-  List<String>friends;
-
-
-
-  Future<List<Post>> search(String text) async {
-    await Future.delayed(Duration(seconds: 1));
-
-    //if (isReplay) return [Post("Replaying !", "Replaying body")];
-    //if (text.length == 5) throw Error();
-    //if (text.length == 6) return [];
-
-    List<String>friends =["Toinker","Toink",'Amazing','Line','Hamed'];
-
-    List<Post> posts = [Post("Line","TandPineasdasdasd"),Post("Anton Due", "Elsker også kage"),Post("Hussein Miari", "Elsker kage"),Post("Hamed Vadai", "Elsker også kageee")];
-    
-    friends.where((friend)=> friend.startsWith('A'));
-    /*var random = new Random();
-    for (int i = 0; i < 10; i++) {
-      posts.add(Post("$text $i", "body random number : ${random.nextInt(100)}"));
-    }*/
-    return posts;
-  }
-
+class Friends extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[900],
-      body: SafeArea(
-        child: SearchBar<Post>(
-          hintText: "Search",
-          textStyle: TextStyle(color: Colors.greenAccent),
-          iconActiveColor: Colors.greenAccent,
-          searchBarPadding: EdgeInsets.symmetric(horizontal: 10),
-          headerPadding: EdgeInsets.symmetric(horizontal: 10),
-          listPadding: EdgeInsets.symmetric(horizontal: 10),
-          onSearch: search,
-          searchBarController: _searchBarController,
-          placeHolder: Text("placeholder",),
-          cancellationWidget: Text("Cancel",style: TextStyle(color: Colors.greenAccent),),
-          emptyWidget: Text("empty"),
-          indexedScaledTileBuilder: (int index) => ScaledTile.count(2,0.4), // How results look
-          header: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              RaisedButton(
-                color: Colors.blueGrey[900],
-                child: Text("sort",style: TextStyle(color: Colors.greenAccent),),
-                onPressed: () {
-                  //friends.where((friend)=> friend.startsWith('A'));
-                  _searchBarController.sortList((Post a, Post b) {
-                    return a.body.compareTo(b.body);
-                  });
-                },
-              ),
-              RaisedButton(
-                color: Colors.blueGrey[900],
-                child: Text("Desort",style: TextStyle(color: Colors.greenAccent),),
-                onPressed: () {
-                  _searchBarController.removeSort();
-                },
-              ),
-              RaisedButton(
-                color: Colors.blueGrey[900],
-                child: Text("Replay",style: TextStyle(color: Colors.greenAccent),),
-                onPressed: () {
-                  isReplay = !isReplay;
-                  _searchBarController.replayLastSearch();
-                },
-              ),
-            ],
-          ),
-
-          onCancelled: () {
-            print("Cancelled triggered");
-          },
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-          crossAxisCount: 2,
-          onItemFound: (Post post, int index) {
-            return Container(
-              color: Colors.greenAccent,
-              child: ListTile(
-                title: Text(post.title),
-                isThreeLine: true,
-                subtitle: Text(post.body),
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => Detail()));
-                },
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class Detail extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      backgroundColor: Colors.blueGrey[900],
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () => Navigator.of(context).pop(),
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: const Text('ExpansionTile',style: TextStyle(color: Colors.greenAccent),),
+          actions: <Widget>[
+            FlatButton.icon(
+                icon: Icon(Icons.search,color: Colors.greenAccent,),
+                label: Text(""),
+              onPressed: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => Search()));
+              },
             ),
-            Text("Detail"),
+
           ],
+
         ),
-      ),
+        body: ListView.builder(
+
+          itemBuilder: (BuildContext context, int index) =>
+              EntryItem(data[index],),
+          itemCount: data.length,
+        ),
+      );
+  }
+}
+
+// One entry in the multilevel list displayed by this app.
+class Entry {
+  Entry(this.title, [this.children = const <Entry>[]]);
+
+  final String title;
+  final List<Entry> children;
+}
+
+// The entire multilevel list displayed by this app.
+final List<Entry> data = <Entry>[
+  Entry(
+    'Incoming Friend Requests',
+    <Entry>[
+      Entry('Section A0'),
+      Entry('Section A1'),
+      Entry('Section A2'),
+    ],
+  ),
+  Entry(
+    'Friendslist',
+    <Entry>[
+      Entry('Section B0'),
+      Entry('Section B1'),
+    ],
+  ),
+  Entry(
+    'Outgoing Friend Requests',
+    <Entry>[
+      Entry('Section C0'),
+      Entry('Section C1'),
+      Entry('Section C2',),
+    ],
+  ),
+];
+
+// Displays one Entry. If the entry has children then it's displayed
+// with an ExpansionTile.
+class EntryItem extends StatelessWidget {
+  const EntryItem(this.entry);
+
+  final Entry entry;
+  Widget _buildTiles(Entry root) {
+
+    if (root.children.isEmpty) return ListTile(title: Text(root.title,style: TextStyle(color: Colors.greenAccent),));
+    return ExpansionTile(
+      backgroundColor: Colors.blueGrey[900],
+      key: PageStorageKey<Entry>(root),
+      title: Text(root.title,style: TextStyle(fontSize: 20 ,fontWeight: FontWeight.bold,color: Colors.greenAccent),),
+      children: root.children.map(_buildTiles).toList(),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(entry,);
   }
 }
