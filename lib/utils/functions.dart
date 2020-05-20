4010a56dd8da7db318bc598336403ad94bcacaf2
Hussein <hkmiari@ruc.dk>
2020-05-20 20:09:41 +0200

    Made challenges_history page.                                                                  Edited showbuttons function so the popup window has another color                                                                 Edited showbuttons function so the popup window has another color

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -6,7 +6,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 double calculateBMI (int weight, int height){
-  return double.parse((weight/pow(height/100,2)).toStringAsFixed(1));
+  double bmi;
+  return bmi = double.parse((weight/pow(height/100,2)).toStringAsFixed(1));
 }
 
 /*Color challengeColor(){
